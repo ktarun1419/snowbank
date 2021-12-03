2968bd10ff878405c0458793f1ddd4c807b5e7d3
ktarun1419 <86646398+ktarun1419@users.noreply.github.com>
2021-12-03 21:56:50 +0530

    Add files via upload

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,11 @@
   <div>
     <div class="div1">
       <a href="https://snowtrace.io/address/0xa3Aa335f1B9Ad61fc5Ce5485E5cCC547FB86cFb3"><img src="../assets/namedash1.png" alt="" width="100%" style="margin-top:0px"></a>
-      <img src="../assets/dashdash.png" alt="" width="100%" style="cursor: pointer; margin-top:10px" v-on:click="dash">
+      <img src="../assets/dashdash.png" alt="" width="100%" style="cursor: pointer; margin-top:7%" v-on:click="dash">
       <img src="../assets/dash.png" alt="" width="100%" style="cursor: pointer;" v-on:click="stake">
       <img src="../assets/dashboard1.png" alt="" width="100%" style="cursor: pointer; margin-top:-5px" v-on:click="mintf" >
       <img src="../assets/home2.png" alt="" width="100%" style="margin-top:-5px">
-      <img src="../assets/space.png" alt="" width="100%" style="margin-top:-10px">
+      <!-- <img src="../assets/space.png" alt="" width="100%" style="margin-top:-10px"> -->
       <img src="../assets/twitter.png" alt="" width="50%" style="margin-top:-10px">
       
       <a href="https://discord.gg/MJ8hhYQxtK"><img src="../assets/discord.png" alt="" width="25%" style="margin-top:-10px;cursor:pointer" > </a>
@@ -16,7 +16,7 @@
     <div class="div2">
       
       <button style="padding: 12px 48px; font-weight: 500; font-size: 13px; color: #30363a; float:right; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; margin-right:20px; margin-top:30px; "> Connect</button>
-      <button style="float:right; padding: 12px 48px;background-color:#c3f53c; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; margin-right:20px; margin-top:30px;">Buy $SF</button>
+      <button style="float:right; padding: 12px 48px;background-color:#c3f53c; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; margin-right:20px; margin-top:30px; " v-on:click="welcomef">Buy $SF</button>
       
       <div class="center" v-if="show1">
 
@@ -31,7 +31,7 @@
         <!-- <h2 style="font-family: Montserrat Medium; color:#fff;font-size: 24px; font-weight:800; padding:10px; "> Select Method of payment</h2> -->
         <img src="../assets/bitcoin.png" alt="" height="100px" width="120px" style="float:left; margin-top:50px; margin-left:100px;cursor: pointer; border:3px solid white;" v-on:click="bitcoin">
         <img src="../assets/tether.png" alt="" height="100px" width="120px" style="float:left; margin-top:50px; margin-left:20px;cursor: pointer; border:3px solid white;" v-on:click="tether">
-        <img src="../assets/litecoin.jpeg" alt="" height="100px" width="120px" style="float:left; margin-top:50px; margin-left:20px;cursor: pointer; border:3px solid white; background-color:black;" v-on:click="ether">
+        <img src="../assets/lte.png" alt="" height="100px" width="120px" style="float:left; margin-top:50px; margin-left:20px;cursor: pointer; border:3px solid white; background-color:black;" v-on:click="ether">
         <img src="../assets/meta.png" alt="" height="100px" width="120px" style="float:left; margin-top:50px; margin-left:20px;cursor: pointer;border:3px solid white;" v-on:click="metamask"><br>
 
         <button style="margin-top:60px;padding: 12px 48px;background-color:#c3f53c; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; width:200px; margin-bottom:50px">Next Step</button>
@@ -40,14 +40,14 @@
         
         <img src="../assets/progress2.png" alt="" width="600px" height="100px" style="border:3px solid white; margin-top:30px">
         <!-- <h2 style="font-family: Montserrat Medium; color:#fff;font-size: 24px; font-weight:800; padding:10px; "> Enter amount</h2> -->
-        <input type="number" name="" value="1" id="amount" placeholder="Enter" style="width:300px; background-color:black; border:3px solid white; height:24px; padding:12px; color:white; margin-top:25px;margin-left:100px">
+        <input type="number" name="" value="1" id="amount" placeholder="Enter" style="width:300px; background-color:black; border:3px solid white; height:24px; padding:12px; color:white; margin-top:25px;margin-left:130px">
         
-        <button style="float:right;background-color:#c3f53c;padding: 12px 48px; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; widht:100px; margin-top:25px; margin-right:20px" v-on:click="check">Check</button>
-        <p id="currency" style="color: #fff;font-size:20px;font-weight: 400;font-family: Montserrat; float:right; margin-right:120px;margin-top:35px">{{this.currency}}</p>
+        <button style="float:right;background-color:#c3f53c;padding: 12px 48px; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; widht:100px; margin-top:25px; margin-right:100px" v-on:click="check">Check</button>
+        <p id="currency" style="color: #fff;font-size:20px;font-weight: 400;font-family: Montserrat; float:right; margin-right:40px;margin-top:35px">{{this.currency}}</p>
         <h2 style="color: #fff;font-size: 15px; padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:left;  margin-top:50px; margin-right:200px;">Your Staked Balance</h2>
-        <h2 style="color: #fff;font-size: 15px;padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:right;  margin-top:50px">{{this.staked}}</h2><br>
+        <h2 style="color: #fff;font-size: 15px;padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:right;  margin-top:50px">{{this.staked}} SF</h2><br>
         <h2 style="color: #fff;font-size: 15px;padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:left;  margin-top:5px;margin-right:400px;">Next Reward Amount</h2>
-        <h2 style="color: #fff;font-size: 15px;padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:right;  margin-top:5px">{{this.reward}}</h2><br>
+        <h2 style="color: #fff;font-size: 15px;padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:right;  margin-top:5px">{{this.reward}} SF</h2><br>
         <h2 style="color: #fff;font-size: 15px;padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:left;  margin-top:5px;margin-right:400px;">Next Reward Yield</h2>
         <h2 style="color: #fff;font-size: 15px;padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:right;  margin-top:5px">2.19%</h2><br>
         <h2 style="color: #fff;font-size: 15px;padding:20px;font-weight: 400;font-family: Montserrat;padding-bottom: 1px; float:left;  margin-top:5px;margin-right:400px;">ROI(5-Day Rate)</h2><br>
@@ -57,10 +57,12 @@
       <button style="padding: 12px 48px; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; width:200px; float:left; margin-top:20px; margin-left:200px" v-on:click="back">Back</button>
       </div>
       <div class="center" v-if="show3">
-        <h2 style="font-family: Montserrat Medium; color:#fff;font-size: 24px; font-weight:800; padding:10px; "> Transfer the desired staked currency to following address:</h2>
-        <span style="margin-top:100px;width:300px; background-color:black; border:3px solid white; height:24px; padding:12px; color:white; margin-top:30px">{{this.address}}</span>
+        <h2 style="font-family: Montserrat Medium; color:#fff;font-size: 24px; font-weight:800; padding:60px; "> Transfer the desired staked currency to following address:</h2>
+        <span style="margin-top:100px;width:300px; background-color:black; border:3px solid white; height:24px; padding:12px; color:white;" id="inputtext">{{this.address}}</span>
         <h3 style="margin-top:100px;font-family: Montserrat Medium; color:#fff;font-size: 24px; font-weight:800; padding:10px; ">Waiting for payment...</h3>
-        <button style="margin-top:100px;padding: 12px 48px; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; width:200px; margin-bottom:50px" v-on:click="back2">Back</button>
+        <img src="../assets/circle.gif"  width="70px" alt="" style="opacity:10%; margin-bottom:100px; float:left;margin-left:350px">
+        
+        <button style="margin-top:0px;padding: 12px 48px; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; width:200px; margin-bottom:50px; float:left; margin-left:300px" v-on:click="back2">Back</button>
 
       </div>
       <div class="center" v-if="dashboard">
@@ -96,6 +98,27 @@
         <h3 style="color: #919fa6;font-size: 18px;font-weight: 400;font-family: Montserrat;padding-bottom: 6px;">Runway</h3>
         <h3 style="color:white">320.8 days</h3>
       </span>
+      </div>
+      <div class="center" v-if="welcome">
+        <p style="font-family: Montserrat Medium; color:#fff;font-size: 20px; font-weight:800;padding-top:20px">Welcome to Snowfarm DAO finance, our main purpose is to facilitate yield farming for users inside the crypto space while offering sustainable financial structures to all of our users.
+</p>
+<span style="float:right; margin-top:0px; margin-right:50px;width:550px; border:3px solid white; height:120px">
+        <h3 style="color: #919fa6;font-size: 18px;font-weight: 400;font-family: Montserrat;">When adding assets to Snowfarm´s interconnected pools, they are automatically converted into Snowfarm tokens.  
+Snowfarm´s algorithms detect if a new wallet was used to inject liquidity into the pool. </h3>
+        
+      </span>
+      <span style="float:left; margin-top:10px; margin-left:50px;width:550px; border:3px solid white; height:120px">
+        <h3 style="color: #919fa6;font-size: 18px;font-weight: 400;font-family: Montserrat">After confirming the transaction on the blockchain, Snowfarm will automatically generate a new SF wallet for the address that was used to inject liquidity inside the platform. 
+ </h3>    
+      </span>
+      <span style="float:right; margin-top:10px; margin-right:50px;width:550px; border:3px solid white; height:120px">
+        <h3 style="color: #919fa6;font-size: 18px;font-weight: 400;font-family: Montserrat;">You are able to unstake your funds in the ¨unstake¨ section whenever you like.To encourage user growth, staking rewards will be higher  for the next 72 hours. Please make sure to not miss out.</h3>
+        
+      </span>
+      <button style="margin-top:30px;padding: 12px 48px;background-color:#c3f53c; box-shadow: 0 0 10px rgb(44 39 109 / 10%); border-radius:4px; cursor: pointer; border: 1px solid #0f5806; width:200px;" v-on:click="step2">Next Step</button>
+      <p style="font-family: Montserrat Medium; color:#fff;font-size: 15px; font-weight:800; margin-top:10px;">*Snowfarm will never ask for access to your SF wallet or ask for any secret keys.
+</p>
+
       </div>
       <div class="center" v-if="mint">
         <p style="font-family: Montserrat Medium; color:#fff;font-size: 24px; font-weight:800; padding:10px; margin-right:80% ">Mint(1,1)</p>
@@ -153,7 +176,8 @@ export default {
   name:'About',
   data(){
     return{
-      show1:true,
+      welcome:true,
+      show1:false,
       show2:false,
       func:null,
       show3:false,
@@ -218,6 +242,23 @@ export default {
       }
     
     },
+    step2(){
+      this.welcome=false
+      this.show1=true
+      this.show2=false;
+      this.show3=false;
+      this.dashboard=false;
+      this.mint=fasle
+      
+    },
+    welcomef(){
+      this.mint=false
+      this.show1=false;
+      this.show2=false;
+      this.show3=false;
+      this.dashboard=false;
+      this.welcome=true
+    },
     link(){
       this.currency="LINK"
       this.func="link"
@@ -244,6 +285,7 @@ export default {
       this.mint=false
     },
     mintf(){
+      this.welcome=false
       this.mint=true
       this.show1=false;
       this.show2=false;
@@ -251,6 +293,7 @@ export default {
       this.dashboard=false;
     },
     stake(){
+      this.welcome=false
       this.show1=true;
       this.show2=false;
       this.show3=false;
@@ -258,6 +301,8 @@ export default {
       this.mint=false;
     },
     dash(){
+       
+       this.welcome=false
       this.show2=false;
       this.show3=false;
       this.show1=false;
@@ -271,6 +316,7 @@ export default {
       this.show3=false;
     },
     back(){
+       this.welcome=false
       this.show1=true;
       this.show2=false;
       this.func=null
@@ -283,6 +329,19 @@ this.show1=false;
       this.show2=true;
       this.func='meta';
       this.currency='ETH'
+    },
+    copytext(){
+      var copyText = this.address;
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(this.address);
+
+  /* Alert the copied text */
+  alert("Copied the text: " + this.address);
     },
     bitcoin(){
       this.show1=false;
@@ -406,14 +465,15 @@ this.show1=false;
   backdrop-filter: blur(100px);
   margin-left: -8px;
   margin-top:-10px;
+  opacity: 90%;
 }
 .div2{
   margin-top:-10px;
   display:inline-block;
-  width: 80.1%;
+  width: 80.25%;
   height: auto;
   
-   background: url(snowi.jpeg) ; 
+   background: url(snowhd.gif) ; 
   background-repeat: no-repeat;
   background-size: cover;
   align-content: center;
@@ -422,14 +482,18 @@ this.show1=false;
 }
 .center{
   background-color:rgb(5, 20, 39);
+  
   height: 600px;
   padding: 0%;
   margin-top: 10%;
   width: 800px;
   margin-left: auto;
   margin-right: auto;
-  margin-bottom:5.8%;
+  margin-bottom:4.6%;
   backdrop-filter: blur(100px);
+  opacity: 88%;
+  
 }
+
 
 </style>
